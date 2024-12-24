3d3ee593147f8cc6a3ba838518a8e0d50dfeacb9
Rakshit Upadhyay <rakshitupadhyay.2004@gmail.com>
2024-12-24 11:53:46 +0530

    Service Created with type=entity-auto

---
+++ service/MoquiTrainingService.groovy
@@ -0,0 +1,16 @@
+import static org.moqui.util.ObjectUtilities.*
+import static org.moqui.util.CollectionUtilities.*
+import static org.moqui.util.StringUtilities.*
+import java.sql.Timestamp
+import java.sql.Time
+import java.time.*
+// these are in the context by default: ExecutionContext ec, Map<String, Object> context, Map<String, Object> result
+ec.logger.log("info", """==========context before=${context}""", null)
+if (true) {
+    ec.service.sync().name("create#org.moqui.training.MoquiTraining")
+            .parameters(context).call()
+
+    if (ec.message.hasError()) return
+}
+// make sure the last statement is not considered the return value
+return;
